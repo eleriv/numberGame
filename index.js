718933ac608fe7830b4380474b07a4da23fd4d30
Eleri <elviks@ttu.ee>
2020-11-30 16:33:22 +0200

    Number Game

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import npm packages
 const prompt = require('prompt-sync')();
+
 // Generates a random number
 const random = Math.floor(Math.random() * 101);
 
@@ -11,19 +12,23 @@ console.log("Guess a randomly generated number that is between 1 and 100.");
 
 // Lasts until a user has guessed the random number
 while (win == false) {
+
     // Asks a user a number
     const userInput = prompt("Your guess: ");
 
     // If the guessed number was too low
     if (userInput > random) {
         console.log("The number is lower");
+
     // If the guessed number was too high
     } else if (userInput < random) {
         console.log("The number is higher");
+
     // If the guessed number was correct
     } else if (userInput == random) {
         console.log("Correct! Thank you for playing!");
         win = true;
+
     // Ends the game If the user types 'exit'
     } else if (userInput === "exit") {
         console.log("Better luck next time! The number was " + random);
